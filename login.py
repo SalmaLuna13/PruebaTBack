529e3aff38261a01e3cccded383b5bc3954117d3
SalmaLuna13 <luna.arenas.22046@itsmante.edu.mx>
2025-05-12 13:52:34 -0600

    metodo post

--- login.py
+++ login.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 bp = Blueprint('auth', __name__)
 
 # Habilitar CORS globalmente para todas las rutas
-CORS(app, resources={r"/*": {"origins": "https://salmaluna13.github.io"}})
+CORS(app) 
 
 app.register_blueprint(bp, url_prefix='/auth')
 
